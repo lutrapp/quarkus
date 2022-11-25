8ad85d19ad88aa16468c0ef2c60d8376ea6f7cdd
Trapp <luciane.trapp@accenture.com>
2022-11-25 15:23:09 -0300

    adicionados testes

--- auladia23/src/test/java/acc/br/FrutasResourceTest.java
+++ auladia23/src/test/java/acc/br/FrutasResourceTest.java
@@ -1,6 +1,7 @@
 package acc.br;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 import javax.inject.Inject;
 
@@ -10,30 +11,42 @@ import acc.br.model.Fruta;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 
-
-
 @QuarkusTest
 public class FrutasResourceTest {
+	@Test
+    public void frutasEndpointTest() {
+        given().contentType(ContentType.JSON)
+                .when().get("/frutas")
+                .then()
+                .statusCode(200);
+    }
+	
+	@Test
+	public void novaFrutaEndpointTest() {
+			Fruta fruta = new Fruta();
+	        given().contentType(ContentType.JSON).body(fruta)
+	                .when().post("/frutas")
+	                .then()
+	                .statusCode(204);
+	    }
+	
+    @Test
+    public void frutaPorIdEndpointTest() {
+    	Fruta fruta = new Fruta();
+    	Long id = 100L;
+        given().contentType(ContentType.JSON).body(fruta)
+          .when().get("/frutas/"+id)
+          .then()
+             .statusCode(200);
+    }
    
-	//fazer depois com Mockito
-//    @Test
-//    public void novaFrutaEndpointTest() {
-//    	Fruta fruta = new Fruta();
-//        given().contentType(ContentType.JSON).body(fruta)
-//          .when().post("/frutas")
-//          .then()
-//             .statusCode(204);
-//             
-//    }
-//    @Test
-//    public void apagaFrutaEndpointTest() {
-//    	Fruta fruta = new Fruta();
-//    	Long id = 1L;
-//        given().contentType(ContentType.JSON).body(fruta)
-//          .when().delete("/frutas/{id});"
-//          .then()
-//             .statusCode(204);
-//             
-//    }
-
+    @Test
+    public void apagaFrutaEndpointTest() {
+    	Fruta fruta = new Fruta();
+    	Long id = 100L;
+        given().contentType(ContentType.JSON).body(fruta)
+          .when().delete("/frutas/"+id)
+          .then()
+             .statusCode(204);
+    }
 }
