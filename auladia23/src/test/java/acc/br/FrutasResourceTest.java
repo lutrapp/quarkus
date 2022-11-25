7bc4f1e4cce108416476169652e5f99538cf99c4
Trapp <luciane.trapp@accenture.com>
2022-11-25 15:27:41 -0300

    ajuste

--- auladia23/src/test/java/acc/br/FrutasResourceTest.java
+++ auladia23/src/test/java/acc/br/FrutasResourceTest.java
@@ -15,38 +15,42 @@ import io.restassured.http.ContentType;
 public class FrutasResourceTest {
 	@Test
     public void frutasEndpointTest() {
-        given().contentType(ContentType.JSON)
-                .when().get("/frutas")
-                .then()
-                .statusCode(200);
+        given()
+        .contentType(ContentType.JSON)
+        .when().get("/frutas")
+        .then()
+        .statusCode(200);
     }
 	
 	@Test
 	public void novaFrutaEndpointTest() {
-			Fruta fruta = new Fruta();
-	        given().contentType(ContentType.JSON).body(fruta)
-	                .when().post("/frutas")
-	                .then()
-	                .statusCode(204);
+		Fruta fruta = new Fruta();
+        given()
+        .contentType(ContentType.JSON).body(fruta)
+        .when().post("/frutas")
+        .then()
+        .statusCode(204);
 	    }
 	
     @Test
     public void frutaPorIdEndpointTest() {
     	Fruta fruta = new Fruta();
     	Long id = 100L;
-        given().contentType(ContentType.JSON).body(fruta)
-          .when().get("/frutas/"+id)
-          .then()
-             .statusCode(200);
+        given()
+        .contentType(ContentType.JSON).body(fruta)
+        .when().get("/frutas/"+id)
+        .then()
+        .statusCode(200);
     }
    
     @Test
     public void apagaFrutaEndpointTest() {
     	Fruta fruta = new Fruta();
     	Long id = 100L;
-        given().contentType(ContentType.JSON).body(fruta)
-          .when().delete("/frutas/"+id)
-          .then()
-             .statusCode(204);
+        given()
+        .contentType(ContentType.JSON).body(fruta)
+        .when().delete("/frutas/"+id)
+        .then()
+        .statusCode(204);
     }
 }
